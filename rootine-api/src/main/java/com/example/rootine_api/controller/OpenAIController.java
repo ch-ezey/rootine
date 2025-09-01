714ff9a4a039e0aa3376d34b9e3e2035f43af756
Cheta Ezenwoye <88289210+ch-ezey@users.noreply.github.com>
2025-09-01 02:19:54 +0100

    AI Routine Generation, Dockerised

---
+++ rootine-api/src/main/java/com/example/rootine_api/controller/OpenAIController.java
@@ -0,0 +1,22 @@
+package com.example.rootine_api.controller;
+
+import com.example.rootine_api.service.OpenAIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/openai")
+@CrossOrigin
+public class OpenAIController {
+
+    @Autowired
+    private OpenAIService openAIService;
+
+    @GetMapping("/response")
+    private ResponseEntity<String> getOpenAIResponse(@RequestBody String prompt){
+        String response = openAIService.generateRoutine(prompt);
+        return ResponseEntity.ok(response);
+    }
+
+}
